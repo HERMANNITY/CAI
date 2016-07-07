b67e149655b217de63bded8c1631d039502c3a40
Junhan Liu <junhanliu2015@u.northwestern.edu>
2016-07-07 14:07:13 -0500

    7/7/2016

--- MvcTraining/MvcTraining/Controllers/CustomerBaseViewModelsController.cs
+++ MvcTraining/MvcTraining/Controllers/CustomerBaseViewModelsController.cs
@@ -41,6 +41,16 @@ namespace MvcTraining.Controllers
             return View();
         }
 
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public new ActionResult Profile()
+        {
+            return View();
+        }
+
         // POST: CustomerBaseViewModels/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
